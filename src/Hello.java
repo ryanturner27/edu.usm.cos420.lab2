39ecf0310b4fd485b3a336abab85565d0ef54ee0
Ryan Turner <ryan.a.turner@maine.edu>
2016-02-08 00:30:44 -0500

    Added Goodbye Everyone!

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,11 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Here Ye, Here Ye : Important announcement!");
+		
 		System.out.println("Hello World!");
+		
+			
+		System.out.println("Goodbye Everyone!");
 	}
-
 }
